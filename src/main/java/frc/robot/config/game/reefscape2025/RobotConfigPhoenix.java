14b3dd79b3d9a5e5de37bdf4732f904f67af04c1
Ethan Porter <emanporter2009@gmail.com>
2025-01-22 19:48:21 -0500

    Updated camera info

--- src/main/java/frc/robot/config/game/reefscape2025/RobotConfigPhoenix.java
+++ src/main/java/frc/robot/config/game/reefscape2025/RobotConfigPhoenix.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import frc.robot.Robot;
 import frc.robot.subsystems.implementations.drive.DriveBase;
 import frc.robot.subsystems.implementations.drive.DriveSwerveYAGSL;
 import frc.robot.subsystems.interfaces.Drive;
@@ -23,16 +24,13 @@ public class RobotConfigPhoenix extends RobotConfig {
 
     vision.addCamera(
         new Camera(
-            "photonvision",
+            "my-first-photonvision",
             new Transform3d(
-                new Translation3d(0.221, 0, .164),
-                new Rotation3d(0, Units.degreesToRadians(-20), Units.degreesToRadians(0)))));
+              new Translation3d(Units.inchesToMeters(13.25), 0, Units.inchesToMeters(6.25)),
+              new Rotation3d(0, Units.degreesToRadians(30), 0))));
 
-    // actual camera
-    // "my-first-photonvision",
-    //         "1182",
-    //         new Transform3d(
-    //             new Translation3d(Units.inchesToMeters(13.25), 0, Units.inchesToMeters(6.25)),
-    //             new Rotation3d(0, Units.degreesToRadians(30), 0))));
+    // if (Robot.isSimulation()) {
+    //   vision.enableSimulation(() -> RobotConfig.drive.getPose(), false);
+    // }
   }
 }
