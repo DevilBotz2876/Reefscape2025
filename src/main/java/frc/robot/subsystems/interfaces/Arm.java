417a88637989bb2e854e08d938fb25bbc156e659
Ethan Porter <emanporter2009@gmail.com>
2025-01-25 15:13:09 -0500

    spotless

--- src/main/java/frc/robot/subsystems/interfaces/Arm.java
+++ src/main/java/frc/robot/subsystems/interfaces/Arm.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.interfaces;
 
-import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
