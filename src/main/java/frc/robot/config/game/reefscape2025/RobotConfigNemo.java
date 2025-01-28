4117a69baa4119895c9402c6ca8fa66ee3a93ac2
Nilesh Agarwalla <nilesh@alum.mit.edu>
2025-01-27 21:04:14 -0500

    spotlessApply

--- src/main/java/frc/robot/config/game/reefscape2025/RobotConfigNemo.java
+++ src/main/java/frc/robot/config/game/reefscape2025/RobotConfigNemo.java
@@ -1,13 +1,8 @@
 package frc.robot.config.game.reefscape2025;
 
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.util.Units;
 import frc.robot.subsystems.implementations.drive.DriveBase;
 import frc.robot.subsystems.implementations.drive.DriveSwerveYAGSL;
 import frc.robot.subsystems.interfaces.Drive;
-import frc.robot.subsystems.interfaces.Vision.Camera;
 
 /* Override Nemo specific constants here */
 public class RobotConfigNemo extends RobotConfig {
