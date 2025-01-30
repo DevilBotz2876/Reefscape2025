d685e7434b019638c3e1bc30cfa316fe80b3ca2f
joshuman <joshuamanoj2008@gmail.com>
2025-01-29 19:53:11 -0500

    Ran spotless Apply

--- src/main/java/frc/robot/config/game/reefscape2025/RobotConfigPhoenix.java
+++ src/main/java/frc/robot/config/game/reefscape2025/RobotConfigPhoenix.java
@@ -2,12 +2,10 @@ package frc.robot.config.game.reefscape2025;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import frc.robot.commands.common.arm.ArmCommand;
 import frc.robot.commands.common.arm.ArmToPosition;
 import frc.robot.commands.common.elevator.ElevatorCommand;
 import frc.robot.subsystems.implementations.drive.DriveBase;
@@ -34,7 +32,8 @@ public class RobotConfigPhoenix extends RobotConfig {
                 new Translation3d(0.221, 0, .164),
                 new Rotation3d(0, Units.degreesToRadians(-20), Units.degreesToRadians(0)))));
 
-    NamedCommands.registerCommand("Move Elevator", new ElevatorCommand(RobotConfig.elevator, () -> 1.0).withTimeout(0.05));
+    NamedCommands.registerCommand(
+        "Move Elevator", new ElevatorCommand(RobotConfig.elevator, () -> 1.0).withTimeout(0.05));
     NamedCommands.registerCommand("Move Arm", new ArmToPosition(RobotConfig.arm, () -> 45.0));
     autoChooser = AutoBuilder.buildAutoChooser("Sit Still");
   }
