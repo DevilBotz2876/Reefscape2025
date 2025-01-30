f876bbfd4886aec6d1b17829f0d5489733fe3bfa
Ethan Porter <emanporter2009@gmail.com>
2025-01-29 19:20:34 -0500

    spotless

--- src/main/java/frc/robot/subsystems/controls/elevator/ElevatorControls.java
+++ src/main/java/frc/robot/subsystems/controls/elevator/ElevatorControls.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.controls.elevator;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.common.elevator.ElevatorVolts;
@@ -10,13 +9,15 @@ public class ElevatorControls {
   public static void setupController(Elevator elevator, CommandXboxController controller) {
     SubsystemBase elevatorSubsystem = (SubsystemBase) elevator;
     elevatorSubsystem.setDefaultCommand(
-        new ElevatorVolts(elevator, () -> {
-          if(controller.pov(0).getAsBoolean()) {
-            return 1.0;
-          } else if(controller.pov(180).getAsBoolean()) {
-            return -1.0;
-          }
-          return 0.0;
-        }));
+        new ElevatorVolts(
+            elevator,
+            () -> {
+              if (controller.pov(0).getAsBoolean()) {
+                return 1.0;
+              } else if (controller.pov(180).getAsBoolean()) {
+                return -1.0;
+              }
+              return 0.0;
+            }));
   }
 }
