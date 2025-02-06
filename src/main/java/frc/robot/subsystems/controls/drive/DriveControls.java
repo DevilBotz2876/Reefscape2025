2d7dd1ee7f1237f888f937b7787477c769aee78a
joshuman <joshuamanoj2008@gmail.com>
2025-02-05 20:59:27 -0500

    Got on the fly to work sort of. The robot still goes through the reef

--- src/main/java/frc/robot/subsystems/controls/drive/DriveControls.java
+++ src/main/java/frc/robot/subsystems/controls/drive/DriveControls.java
@@ -1,8 +1,14 @@
 package frc.robot.subsystems.controls.drive;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -36,6 +42,26 @@ public class DriveControls {
                     drive.setFieldOrientedDrive(true);
                   }
                 })); // Toggle Drive Orientation
+
+    //                 List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
+    //         new Pose2d(2.0,4.0, Rotation2d.fromDegrees(0)),
+    //         new Pose2d(7.0, 4.0, Rotation2d.fromDegrees(0))
+    // );
+    Pose2d targetPose = new Pose2d(6, 6, Rotation2d.fromDegrees(0));
+    PathConstraints constraints = new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI);
+
+    Command pathfindingCommand =
+        AutoBuilder.pathfindToPose(
+            targetPose,
+            constraints,
+            0.0 // Goal end velocity in meters/sec// Rotation delay distance in meters. This is how
+            // far the robot should travel before attempting to rotate.
+            );
+
+    // Prevent the path from being flipped if the coordinates are already correct
+
+    SmartDashboard.putData("follow", pathfindingCommand);
+    controller.a().whileTrue(pathfindingCommand);
   }
 
   public static void addGUI(Drive drive, ShuffleboardTab tab) {
