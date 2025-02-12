96e582ca70077839bb2038bdd229593947f41860
Nilesh Agarwalla <nilesh@alum.mit.edu>
2025-02-12 16:09:21 -0500

    Motor v2.0 - stub robot example

--- src/main/java/frc/robot/config/game/reefscape2025/RobotConfigStub.java
+++ src/main/java/frc/robot/config/game/reefscape2025/RobotConfigStub.java
@@ -1,46 +1,166 @@
 package frc.robot.config.game.reefscape2025;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
 import frc.robot.Robot;
-import frc.robot.io.implementations.motor.MotorIOStub;
-import frc.robot.subsystems.controls.gizmo.GizmoControls;
+import frc.robot.io.implementations.motor.MotorIOArmStub;
+import frc.robot.io.implementations.motor.MotorIOBase.MotorIOBaseSettings;
+import frc.robot.io.implementations.motor.MotorIOElevatorStub;
+import frc.robot.io.implementations.motor.MotorIOFlywheelStub;
+import frc.robot.subsystems.controls.arm.AlgaeArmControls;
+import frc.robot.subsystems.controls.arm.ClimberArmControls;
+import frc.robot.subsystems.controls.arm.CoralArmControls;
+import frc.robot.subsystems.controls.elevator.ElevatorControlsV2;
+import frc.robot.subsystems.controls.flywheel.FlywheelControls;
 import frc.robot.subsystems.implementations.drive.DriveSwerveYAGSL;
-import frc.robot.subsystems.implementations.gizmo.GizmoSubsystem;
+import frc.robot.subsystems.implementations.motor.ArmMotorSubsystem;
+import frc.robot.subsystems.implementations.motor.ElevatorMotorSubsystem;
+import frc.robot.subsystems.implementations.motor.FlywheelMotorSubsystem;
+import frc.robot.subsystems.interfaces.ArmV2.ArmSettings;
+import frc.robot.subsystems.interfaces.ElevatorV2.ElevatorSettings;
+import frc.robot.subsystems.interfaces.Flywheel.FlywheelSettings;
 
 /* Override Phoenix specific constants here */
 public class RobotConfigStub extends RobotConfig {
-  GizmoSubsystem gizmo;
+  private final ArmMotorSubsystem coralArm;
+  private final ArmMotorSubsystem algaeArm;
+  private final ArmMotorSubsystem climberArm;
+  private final ElevatorMotorSubsystem elevator;
+  private final FlywheelMotorSubsystem algaeIntake;
 
   public RobotConfigStub() {
-    super(false, true, true);
+    super(false, false, false);
 
     drive = new DriveSwerveYAGSL("yagsl/stub");
     if (Robot.isSimulation()) {
       drive.setPose(new Pose2d(new Translation2d(1, 1), new Rotation2d()));
     }
 
-    // We create a GizmoSubsystem using the MotorIOStub implementation of a MotorIO.
-    //
-    // We set inverted=true because we want positive voltage to result in spinning clockwise. The
-    // inversion depends on how the motor is physically connected to the gizmo, so specific to this
-    // robot configuration.
-    //
-    // We set moi=0.025 as a random default, but should be set based on CAD/mech teams calculations
-    // of the actual mechanism
-    //
-    // We set the gear ratio to 50:1 so that the simulation is as accurate as possible.
-    gizmo = new GizmoSubsystem(new MotorIOStub(true, 0.025, 50));
+    {
+      MotorIOBaseSettings motorSettings = new MotorIOBaseSettings();
+      motorSettings.motor.gearing = 50;
+      motorSettings.motor.inverted = false;
+      motorSettings.pid = new PIDController(1.0, 0, 0);
+
+      ArmSettings armSettings = new ArmSettings();
+      armSettings.minAngleInDegrees = -75;
+      armSettings.maxAngleInDegrees = 75;
+      armSettings.startingAngleInDegrees = armSettings.minAngleInDegrees;
+      armSettings.feedforward = new ArmFeedforward(0, 0.222, 0.001, 0);
+      armSettings.color = new Color8Bit(Color.kBlue);
+      armSettings.armLengthInMeters = 0.5;
+      armSettings.armMassInKg = 1.0;
+      armSettings.motor = DCMotor.getNEO(1);
+      armSettings.simulateGravity = true;
+      coralArm =
+          new ArmMotorSubsystem(
+              new MotorIOArmStub(motorSettings, armSettings), "Coral", armSettings);
+    }
+
+    {
+      MotorIOBaseSettings motorSettings = new MotorIOBaseSettings();
+      motorSettings.motor.gearing = 50;
+      motorSettings.motor.inverted = false;
+      motorSettings.pid = new PIDController(0, 0, 0);
+
+      ArmSettings armSettings = new ArmSettings();
+      armSettings.minAngleInDegrees = -15;
+      armSettings.maxAngleInDegrees = 105;
+      armSettings.startingAngleInDegrees = armSettings.maxAngleInDegrees;
+      armSettings.color = new Color8Bit(Color.kGreen);
+      armSettings.feedforward = new ArmFeedforward(0, 0, 0, 0);
+      armSettings.armLengthInMeters = 0.75;
+      armSettings.armMassInKg = 1.0;
+      armSettings.motor = DCMotor.getKrakenX60(1);
+      armSettings.simulateGravity = false;
+
+      algaeArm =
+          new ArmMotorSubsystem(
+              new MotorIOArmStub(motorSettings, armSettings), "Algae", armSettings);
+    }
+
+    {
+      MotorIOBaseSettings motorSettings = new MotorIOBaseSettings();
+      motorSettings.motor.gearing = 50;
+      motorSettings.motor.inverted = false;
+      motorSettings.pid = new PIDController(1, 0, 0);
+
+      ArmSettings armSettings = new ArmSettings();
+      armSettings.minAngleInDegrees = 0;
+      armSettings.maxAngleInDegrees = 90;
+      armSettings.startingAngleInDegrees = armSettings.maxAngleInDegrees;
+      armSettings.color = new Color8Bit(Color.kRed);
+      armSettings.feedforward = new ArmFeedforward(0.0021633, 0.060731, 0.9481, 0);
+      armSettings.armLengthInMeters = 0.5;
+      armSettings.armMassInKg = 0.75;
+      armSettings.motor = DCMotor.getKrakenX60(1);
+      armSettings.simulateGravity = true;
+
+      climberArm =
+          new ArmMotorSubsystem(
+              new MotorIOArmStub(motorSettings, armSettings), "Climber", armSettings);
+    }
+
+    {
+      MotorIOBaseSettings motorSettings = new MotorIOBaseSettings();
+      motorSettings.motor.gearing = 50;
+      motorSettings.motor.inverted = false;
+      motorSettings.motor.drumRadiusMeters = 0.050; // 50 mm radius
+      motorSettings.pid = new PIDController(0, 0, 0);
+
+      ElevatorSettings elevatorSettings = new ElevatorSettings();
+      elevatorSettings.minHeightInMeters = 0.1;
+      elevatorSettings.maxHeightInMeters = 1.0;
+      elevatorSettings.startingHeightInMeters = elevatorSettings.minHeightInMeters;
+      elevatorSettings.color = new Color8Bit(Color.kSilver);
+      elevatorSettings.feedforward = new ElevatorFeedforward(0, 0, 0, 0);
+      elevatorSettings.carriageMassKg = 5.0;
+      elevatorSettings.motor = DCMotor.getKrakenX60(1);
+      elevatorSettings.simulateGravity = true;
+
+      elevator =
+          new ElevatorMotorSubsystem(
+              new MotorIOElevatorStub(motorSettings, elevatorSettings), "Coral", elevatorSettings);
+    }
+
+    {
+      MotorIOBaseSettings motorSettings = new MotorIOBaseSettings();
+      motorSettings.motor.gearing = 1;
+      motorSettings.motor.inverted = false;
+      motorSettings.pid = new PIDController(0, 0, 0);
+
+      FlywheelSettings flywheelSettings = new FlywheelSettings();
+      flywheelSettings.color = new Color8Bit(Color.kPurple);
+      flywheelSettings.feedforward = new SimpleMotorFeedforward(0, 0);
+      flywheelSettings.moiKgMetersSquared = 0.001;
+      flywheelSettings.motor = DCMotor.getNeo550(1);
+
+      algaeIntake =
+          new FlywheelMotorSubsystem(
+              new MotorIOFlywheelStub(motorSettings, flywheelSettings),
+              // new MotorIOStub(settings, simulationSettings),
+              "Algae",
+              flywheelSettings);
+    }
   }
 
   @Override
   public void configureBindings() {
     // Configure the default bindings of the parent class
-    super.configureBindings();
+    // super.configureBindings();
 
-    // Setup Gizmo specific controls and 2D simulation
-    GizmoControls.setupController(gizmo, mainController);
-    GizmoControls.setup2dSimulation(gizmo);
+    CoralArmControls.setupController(coralArm, mainController);
+    AlgaeArmControls.setupController(algaeArm, mainController);
+    ClimberArmControls.setupController(climberArm, mainController);
+    ElevatorControlsV2.setupController(elevator, mainController);
+    FlywheelControls.setupController(algaeIntake, mainController);
   }
 }
