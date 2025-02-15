dc192311462196a02c2ffa4c5f48622f5da73b64
danielcastellarin <daniel.r.castellarin@gmail.com>
2025-02-15 15:30:29 -0500

    Defining temporary camera placement on Nemo

--- src/main/java/frc/robot/config/game/reefscape2025/RobotConfigNemo.java
+++ src/main/java/frc/robot/config/game/reefscape2025/RobotConfigNemo.java
@@ -2,7 +2,11 @@ package frc.robot.config.game.reefscape2025;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import frc.robot.io.implementations.motor.MotorIOBase.MotorIOBaseSettings;
@@ -13,6 +17,7 @@ import frc.robot.subsystems.implementations.drive.DriveSwerveYAGSL;
 import frc.robot.subsystems.implementations.motor.ArmMotorSubsystem;
 import frc.robot.subsystems.interfaces.ArmV2.ArmSettings;
 import frc.robot.subsystems.interfaces.Drive;
+import frc.robot.subsystems.interfaces.Vision.Camera;
 
 /* Override Nemo specific constants here */
 public class RobotConfigNemo extends RobotConfig {
@@ -53,5 +58,30 @@ public class RobotConfigNemo extends RobotConfig {
               // new MotorIOArmStub(motorSettings, armSettings), "Coral", armSettings);
               new MotorIOTalonFx(motorSettings, talonFxSettings), "Coral", armSettings);
     }
+
+    vision.addCamera(
+        new Camera(
+            "my-first-photonvision", // Back camera
+            new Transform3d(
+                new Translation3d(-0.3429, 0, 0.0889),
+                new Rotation3d(0, Units.degreesToRadians(-5), Units.degreesToRadians(180)))));
+    vision.addCamera(
+        new Camera(
+            "left_camera",
+            new Transform3d(
+                new Translation3d(0.0127, 0.3556, 0.0889),
+                new Rotation3d(0, Units.degreesToRadians(-5), Units.degreesToRadians(90)))));
+    vision.addCamera(
+        new Camera(
+            "right_camera",
+            new Transform3d(
+                new Translation3d(0.13335, -0.3556, 0.0889),
+                new Rotation3d(0, Units.degreesToRadians(-5), Units.degreesToRadians(270)))));
+    vision.addCamera(
+        new Camera(
+            "front_camera", // TODO camera too high: place lower on elevator, then fix this tf
+            new Transform3d(
+                new Translation3d(0.08255, -0.2794, 0.9525),
+                new Rotation3d(0, Units.degreesToRadians(30), Units.degreesToRadians(10)))));
   }
 }
