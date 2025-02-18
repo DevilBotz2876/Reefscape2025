5fdb044b3ce2e8a522e64ef2ca8ca6d4e1b0bfbf
Ethan Porter <emanporter2009@gmail.com>
2025-02-18 11:10:40 -0500

    spotless

--- src/main/java/frc/robot/subsystems/controls/flywheel/FlywheelControls.java
+++ src/main/java/frc/robot/subsystems/controls/flywheel/FlywheelControls.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.controls.flywheel;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.commands.common.flywheel.FlywheelCommand;
 import frc.robot.commands.common.flywheel.FlywheelVelocity;
 import frc.robot.commands.common.motor.MotorBringUpCommand;
 import frc.robot.subsystems.interfaces.Flywheel;
@@ -27,17 +26,16 @@ public class FlywheelControls {
     //         }));
 
     flywheelSubsystem.setDefaultCommand(
-      new MotorBringUpCommand(
-          (Motor) flywheel,
-          () -> {
-            if (controller.rightBumper().getAsBoolean()) {
-              return 1.0;
-            } else if (controller.leftBumper().getAsBoolean()) {
-              return -1.0;
-            }
-            return 0.0;
-          }));
-
+        new MotorBringUpCommand(
+            (Motor) flywheel,
+            () -> {
+              if (controller.rightBumper().getAsBoolean()) {
+                return 1.0;
+              } else if (controller.leftBumper().getAsBoolean()) {
+                return -1.0;
+              }
+              return 0.0;
+            }));
 
     // won't work if MotorBringUpCommand is Default Command
     SmartDashboard.putData(
