22acc0e621da5d02f58ddb4f54187c89871b0ae9
Nilesh Agarwalla <nilesh@alum.mit.edu>
2025-02-21 07:42:34 -0500

    Added gui commands for algae arm auto calibrate and up/down

--- src/main/java/frc/robot/subsystems/controls/arm/AlgaeArmControls.java
+++ src/main/java/frc/robot/subsystems/controls/arm/AlgaeArmControls.java
@@ -1,20 +1,46 @@
 package frc.robot.subsystems.controls.arm;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.common.arm.ArmToPosition;
+import frc.robot.commands.common.motor.MotorAutoResetEncoderCommand;
+import frc.robot.commands.common.motor.MotorAutoResetEncoderCommand.MotorAutoResetEncoderSettings;
 import frc.robot.commands.common.motor.MotorBringUpCommand;
+import frc.robot.subsystems.interfaces.Arm;
 import frc.robot.subsystems.interfaces.Motor;
 
 public class AlgaeArmControls {
+  public static class Constants {
+    public static MotorAutoResetEncoderSettings autoZeroSettings =
+        new MotorAutoResetEncoderSettings();
+  }
+
   // RIGHT TRIGGER = up arm
   // LEFT TRIGGER = down arm
-  public static void setupController(Motor motor, CommandXboxController controller) {
-    SubsystemBase motorSubsystem = (SubsystemBase) motor;
-    motorSubsystem.setDefaultCommand(
+  public static void setupController(Arm arm, CommandXboxController controller) {
+    SubsystemBase armSubsystem = (SubsystemBase) arm;
+    armSubsystem.setDefaultCommand(
         new MotorBringUpCommand(
-            motor,
+            (Motor) arm,
+            //        new ArmToPosition(
+            //            arm,
             () -> {
               return (controller.getRightTriggerAxis() - controller.getLeftTriggerAxis());
             }));
+
+    Command autoCalibrateCommand =
+        new MotorAutoResetEncoderCommand((Motor) arm, Constants.autoZeroSettings);
+    SmartDashboard.putData(
+        armSubsystem.getName() + "/Commands/Algae Arm Auto Calibrate ", autoCalibrateCommand);
+
+    SmartDashboard.putData(
+        armSubsystem.getName() + "/Commands/Algae Arm Down",
+        new ArmToPosition(arm, () -> arm.getSettings().minAngleInDegrees));
+
+    SmartDashboard.putData(
+        armSubsystem.getName() + "/Commands/Algae Arm Up",
+        new ArmToPosition(arm, () -> arm.getSettings().maxAngleInDegrees));
   }
 }
