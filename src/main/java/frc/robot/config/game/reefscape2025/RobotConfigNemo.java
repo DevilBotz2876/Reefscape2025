c447940f0e98f43357e6e97f0f13a86b6ab03c94
danielcastellarin <daniel.r.castellarin@gmail.com>
2025-02-21 15:02:17 -0500

    stub cameras

--- src/main/java/frc/robot/config/game/reefscape2025/RobotConfigNemo.java
+++ src/main/java/frc/robot/config/game/reefscape2025/RobotConfigNemo.java
@@ -1,8 +1,14 @@
 package frc.robot.config.game.reefscape2025;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import frc.robot.subsystems.implementations.drive.DriveBase;
 import frc.robot.subsystems.implementations.drive.DriveSwerveYAGSL;
+import frc.robot.subsystems.implementations.vision.VisionSubsystem;
 import frc.robot.subsystems.interfaces.Drive;
+import frc.robot.subsystems.interfaces.Vision.Camera;
 
 /* Override Nemo specific constants here */
 public class RobotConfigNemo extends RobotConfig {
@@ -15,5 +21,32 @@ public class RobotConfigNemo extends RobotConfig {
     Drive.Constants.rotatePidKd = 0.0;
     DriveBase.Constants.rotatePidErrorInDegrees = 1;
     drive = new DriveSwerveYAGSL("yagsl/nemo");
+
+    // Camera placement at WPI practice field (2/21/2025)
+    vision.addCamera(
+        new Camera(
+            "my-first-photonvision", // back
+            new Transform3d(
+                new Translation3d(0.0, 0.0, 0.0),
+                new Rotation3d(0.0, Units.degreesToRadians(0.0), Units.degreesToRadians(0.0)))));
+    vision.addCamera(
+        new Camera(
+          "left",
+          new Transform3d(
+              new Translation3d(0.0, 0.0, 0.0),
+              new Rotation3d(0.0, Units.degreesToRadians(0.0), Units.degreesToRadians(0.0)))));
+    vision.addCamera(
+        new Camera(
+            "right",
+            new Transform3d(
+                new Translation3d(0.0, 0.0, 0.0),
+                new Rotation3d(0.0, Units.degreesToRadians(0.0), Units.degreesToRadians(0.0)))));
+    vision.addCamera(
+        new Camera(
+            "front", // back
+            new Transform3d(
+                new Translation3d(0.0, 0.0, 0.0),
+                new Rotation3d(0.0, Units.degreesToRadians(0.0), Units.degreesToRadians(0.0)))));
+    
   }
 }
