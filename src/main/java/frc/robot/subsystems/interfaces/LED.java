5b33c2e087137e94810b73508060ded5ceb46df2
Rohit VijayKumar <rohitvijayakumar09@gmail.com>
2025-02-25 19:18:16 -0500

    made interface and subsystems for LED

---
+++ src/main/java/frc/robot/subsystems/interfaces/LED.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.interfaces;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+public class LED {
+    public Color getColor() {
+        return new Color(0, 0, 0);
+    }
+}
+
