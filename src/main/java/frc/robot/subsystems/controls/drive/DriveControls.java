2d73146abea8b0f6047fbc815d02612368ec55db
Carter Briscoe <tha.c.bug@gmail.com>
2025-03-05 21:06:32 -0500

    reef positions for other side

--- src/main/java/frc/robot/subsystems/controls/drive/DriveControls.java
+++ src/main/java/frc/robot/subsystems/controls/drive/DriveControls.java
@@ -164,8 +164,8 @@ public class DriveControls {
                 () -> {
                   if (myCoolPoseKeyIdx == 11) myCoolPoseKeyIdx = 2;
                   else myCoolPoseKeyIdx = 11;
-                  //   if (++myCoolPoseKeyIdx == TargetPoseOption.values().length) myCoolPoseKeyIdx
-                  // = 1;
+                    if (++myCoolPoseKeyIdx == TargetPoseOption.values().length) myCoolPoseKeyIdx
+                  = 1;
                   SmartDashboard.putNumber("Chosen Pose Index", myCoolPoseKeyIdx);
                 }));
 
