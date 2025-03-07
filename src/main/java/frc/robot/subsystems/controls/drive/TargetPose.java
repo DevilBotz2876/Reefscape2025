a28890b83403f9fae35b0e63237d320934ec070f
danielcastellarin <daniel.r.castellarin@gmail.com>
2025-03-06 20:36:50 -0500

    fixing positions

--- src/main/java/frc/robot/subsystems/controls/drive/TargetPose.java
+++ src/main/java/frc/robot/subsystems/controls/drive/TargetPose.java
@@ -14,18 +14,18 @@ public enum TargetPose {
   FEEDER_R(1, "FR", "Feeder Right", new Pose2d(1.05, 1, new Rotation2d(50)), 0),
   FEEDER_L(2, "FL", "Feeder Left", new Pose2d(1.05, 7, Rotation2d.fromDegrees(-50)), 0),
   REEF_A(3, "A", "Reef A", new Pose2d(3.15, 4.28, Rotation2d.fromDegrees(0)), -1),
-  REEF_G(9, "G", "Reef G", new Pose2d(5.5, 3.95, Rotation2d.fromDegrees(180)), -1),
-  PROCESSOR(15, "P", "Processor", new Pose2d(6, 0.75, new Rotation2d(270)), 0),
-  REEF_B(4, "B", "Reef B", new Pose2d(3.15, 3.70, Rotation2d.fromDegrees(0)), 0),
+  REEF_B(4, "B", "Reef B", new Pose2d(3.15, 3.70, Rotation2d.fromDegrees(0)), 1),
   REEF_C(5, "C", "Reef C", new Pose2d(3.60, 2.93, Rotation2d.fromDegrees(60)), -1),
-  REEF_D(6, "D", "Reef D", new Pose2d(3.96, 2.74, Rotation2d.fromDegrees(60)), 0),
+  REEF_D(6, "D", "Reef D", new Pose2d(3.96, 2.74, Rotation2d.fromDegrees(60)), 1),
   REEF_E(7, "E", "Reef E", new Pose2d(5.10, 2.75, Rotation2d.fromDegrees(124)), -1),
-  REEF_F(8, "F", "Reef F", new Pose2d(5.36, 2.93, Rotation2d.fromDegrees(124)), 0),
-  REEF_H(10, "H", "Reef H", new Pose2d(5.80, 4.20, Rotation2d.fromDegrees(180)), 0),
+  REEF_F(8, "F", "Reef F", new Pose2d(5.36, 2.93, Rotation2d.fromDegrees(124)), 1),
+  REEF_G(9, "G", "Reef G", new Pose2d(5.5, 3.95, Rotation2d.fromDegrees(180)), -1),
+  REEF_H(10, "H", "Reef H", new Pose2d(5.80, 4.20, Rotation2d.fromDegrees(180)), 1),
   REEF_I(11, "I", "Reef I", new Pose2d(5.30, 5.15, Rotation2d.fromDegrees(-124)), -1),
-  REEF_J(12, "J", "Reef J", new Pose2d(5.00, 5.30, Rotation2d.fromDegrees(-124)), 0),
+  REEF_J(12, "J", "Reef J", new Pose2d(5.00, 5.30, Rotation2d.fromDegrees(-124)), 1),
   REEF_K(13, "K", "Reef K", new Pose2d(4.00, 5.33, Rotation2d.fromDegrees(-60)), -1),
-  REEF_L(14, "L", "Reef L", new Pose2d(3.70, 5.20, Rotation2d.fromDegrees(-60)), 0);
+  REEF_L(14, "L", "Reef L", new Pose2d(3.65, 5.18, Rotation2d.fromDegrees(-60)), 1),
+  PROCESSOR(15, "P", "Processor", new Pose2d(6, 0.75, new Rotation2d(270)), 0);
 
   private int index;
   private String shortName;
