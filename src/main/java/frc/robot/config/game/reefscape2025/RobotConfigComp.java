330e00df86d7ccbea1f6108e03915efd2b9877d2
Ethan Porter <emanporter2009@gmail.com>
2025-03-24 19:37:11 -0400

    change gear ratio

--- src/main/java/frc/robot/config/game/reefscape2025/RobotConfigComp.java
+++ src/main/java/frc/robot/config/game/reefscape2025/RobotConfigComp.java
@@ -106,7 +106,7 @@ public class RobotConfigComp extends RobotConfig {
     // Elevator
     {
       MotorIOBaseSettings motorSettings = new MotorIOBaseSettings();
-      motorSettings.motor.gearing = 9; /* 2x 3:1 gear boxes */
+      motorSettings.motor.gearing = 5; /* 2x 3:1 gear boxes */
       motorSettings.motor.inverted = true;
       // TODO: Get the DIO ports
       // motorSettings.forwardLimitChannel = 7;
